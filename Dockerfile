aab22d42c8267123329078d1e44ecf18ea0b001b
Usama Ahmed <minhasusama66@gmail.com>
2026-05-20 20:08:24 +0500

    Add docker file and exception handling

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+# Use an official, lightweight Python image
+FROM python:3.11-slim
+
+# Prevent Python from writing .pyc files and buffer outputs for cleaner logs
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
+
+# Set the working directory inside the container
+WORKDIR /app
+
+# Install basic system build dependencies required for compiling certain packages
+RUN apt-get update && apt-get install -y --no-install-recommends \
+    build-essential \
+    && rm -rf /var/lib/apt/lists/*
+
+# Copy requirements file first to take advantage of Docker's layer caching
+COPY requirements.txt .
+
+# Installing Python Packages
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the all of the project (app.py, models, scalers, encoders)
+COPY . .
+
+# Gunicorn Port
+EXPOSE 8000
+
+# Run Gunicorn Server
+CMD ["gunicorn", "--bind", "0.0.0.0:8000", "app:app"]
--- app.py
+++ app.py
@@ -13,20 +13,30 @@ ANN = tf.keras.models.load_model(dir / "models/crpann.keras")
 app = Flask(__name__)
 
 def make_prediction(Na, P, temp, humidity, PH):
-    x = np.array([Na, P, temp, humidity, PH])
-    x = x.reshape(1, 5)
-    x = scaler.transform(x)
-    return labelEncoder.inverse_transform([np.argmax(ANN.predict(x))])[0]
+    try:
+        x = np.array([Na, P, temp, humidity, PH])
+        x = x.reshape(1, 5)
+        x = scaler.transform(x)
+        return labelEncoder.inverse_transform([np.argmax(ANN.predict(x))])[0]
+    except Exception as ex:
+        print(ex)
+        return ex
 
 @app.route('/api/predict', methods=['POST'])
-def multiply():
-    f2 = float(request.form['phosphorus'])
-    f1 = float(request.form['nitrogen'])
-    f4 = float(request.form['temperature'])
-    f5 = float(request.form['humidity'])
-    f6 = float(request.form['PH'])
-    crop = make_prediction(f1, f2, f4, f5, f6)
-    return crop
+def index():
+    try:
+        f2 = float(request.form['phosphorus'])
+        f1 = float(request.form['nitrogen'])
+        f4 = float(request.form['temperature'])
+        f5 = float(request.form['humidity'])
+        f6 = float(request.form['PH'])
+        crop = make_prediction(f1, f2, f4, f5, f6)
+        if not crop:
+            raise Exception("Something went wrong")
+        return crop
+    except Exception as ex:
+        print(ex)
+        return "Something went wrong"
 
 if __name__ == '__main__':
     app.run(debug=True)
